39d27ce98739c7a4423f5d627b09266377e18eaa
Yuya Minami <yuya373@me.com>
2018-03-12 22:13:45 +0900

    add buttons to webview in FileDetailView

--- Note/FileDetailViewController.swift
+++ Note/FileDetailViewController.swift
@@ -13,7 +13,12 @@ import WebKit
 class FileDetailViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
-    var file: File?
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var safariButton: UIBarButtonItem!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    var file: File!
+    var fileDetailViewUrl: URL!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,20 +28,17 @@ class FileDetailViewController: UIViewController {
         
         webView.navigationDelegate = self
 
-        file.map { file in
-            if ((file.name ?? "").hasSuffix(".org")) {
-                let url = Bundle.main.url(forResource: "org", withExtension: "html")!
-                let req = URLRequest(url: url)
-                webView.load(req)
-            } else {
-                let url = Bundle.main.url(forResource: "md", withExtension: "html")!
-                let req = URLRequest(url: url)
-                webView.load(req)
-            }
-            navigationItem.title = file.name
-            self.activityIndicatorView.startAnimating()
-            
+        if ((file.name ?? "").hasSuffix(".org")) {
+            self.fileDetailViewUrl = Bundle.main.url(forResource: "org", withExtension: "html")!
+        } else {
+            self.fileDetailViewUrl = Bundle.main.url(forResource: "md", withExtension: "html")!
         }
+        
+        let req = URLRequest(url: fileDetailViewUrl)
+        webView.load(req)
+        navigationItem.title = file.name
+        self.activityIndicatorView.startAnimating()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -54,39 +56,69 @@ class FileDetailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    // MARK: - Actions
+    @IBAction func backButtonTapped(_ sender: UIBarButtonItem) {
+        if (webView.canGoBack) {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func forwardButtonTapped(_ sender: UIBarButtonItem) {
+        if (webView.canGoForward) {
+            webView.goForward()
+        }
+    }
+    
+    @IBAction func safariButtonTapped(_ sender: UIBarButtonItem) {
+        if let url = webView.url {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
 }
 
 extension FileDetailViewController: WKNavigationDelegate {
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        if let file = self.file {
-            DropboxClientsManager.authorizedClient?.files.download(path: file.pathLower!).response { (result, error) in
-                result.map {
-                    let (_, data) = $0
-                    if let str = String(bytes: data, encoding: String.Encoding.utf8) {
-                        let exp = str.trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: "'", with: "\\'").components(separatedBy: .newlines).joined(separator: "\\n")
-                        let js = """
-                            try {
-                                insert('\(exp)');
-                            } catch(e) {
-                                alert(e);
-                            }
-                        """
-                        
-                        print("str: \(exp)")
-                        webView.evaluateJavaScript(js, completionHandler: { result, error in
-                            result.map { print("result: \($0)") }
-                            error.map { print("error: \($0)") }
-                        })
+    private func loadFileContents(webView: WKWebView) {
+        DropboxClientsManager.authorizedClient?.files.download(path: file.pathLower!).response { (result, error) in
+            result.map {
+                let (_, data) = $0
+                if let str = String(bytes: data, encoding: String.Encoding.utf8) {
+                    let exp = str.trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: "'", with: "\\'").components(separatedBy: .newlines).joined(separator: "\\n")
+                    let js = """
+                    try {
+                    insert('\(exp)');
+                    } catch(e) {
+                    alert(e);
                     }
+                    """
+                    
+                    webView.evaluateJavaScript(js, completionHandler: { result, error in
+                        result.map { print("result: \($0)") }
+                        error.map { print("error: \($0)") }
+                    })
                 }
-                error.map {
-                    fatalError("failed to download file: \(file.pathLower ?? "no pathLower!"), error: \($0.description)")
-                }
-                
-                self.activityIndicatorView.stopAnimating()
             }
+            error.map {
+                fatalError("failed to download file: \(self.file.pathLower ?? "no pathLower!"), error: \($0.description)")
+            }
+            
+            self.activityIndicatorView.stopAnimating()
         }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        if (webView.url == self.fileDetailViewUrl) {
+            loadFileContents(webView: webView)
+            safariButton.isEnabled = false
+            navigationItem.title = file.name
+            editButton.isEnabled = true
+        } else {
+            safariButton.isEnabled = true
+            navigationItem.title = webView.title
+            editButton.isEnabled = false
+        }
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
     }
     
 }
